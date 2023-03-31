a41a7ea174fb89e50af63b0052dd954ab9f0e99c
susukida <susukida@loremipsum.co.jp>
2023-03-31 09:59:32 +0900

    Actual initial commit

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/"],
+};
+
+export function middleware(req: NextRequest) {
+  if (process.env.BASIC_AUTH_IS_ENABLED !== "1") {
+    return NextResponse.next();
+  }
+
+  const authorizationHeader = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  if (authorizationHeader) {
+    const authValue = authorizationHeader.split(" ")[1];
+    const [user, password] = atob(authValue).split(":");
+
+    if (
+      user === process.env.BASIC_AUTH_USERNAME &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Auth Required.", {
+    status: 401,
+    headers: {
+      "WWW-authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
